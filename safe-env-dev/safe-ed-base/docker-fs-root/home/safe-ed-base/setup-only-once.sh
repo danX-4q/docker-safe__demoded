8c6a6808287941613ea03dcc963548d62385159d
danX-4q <69082819@qq.com>
2018-11-06 11:16:41 +0800

    deivde safe-env-dev to safe-ed-base and safe-ed; safe-ed-base has been build ok. next build safe-ed

---
+++ safe-env-dev/safe-ed-base/docker-fs-root/home/safe-ed-base/setup-only-once.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+ROOT_DIR=/
+MAIN_DIR_NAME="safe-ed-base"
+
+mkdir -p ${ROOT_DIR}/home/${MAIN_DIR_NAME}/ &&
+cd ${ROOT_DIR}/home/${MAIN_DIR_NAME}/
+
+#######################################
+#######################################
+#######################################
+
+APT_PACK_LIST_FILE=${ROOT_DIR}/home/${MAIN_DIR_NAME}/build-aux/apt-pack-list
+#APT_CACHE_TAR_FILE=${ROOT_DIR}/home/${MAIN_DIR_NAME}/build-aux/cache.apt.archives.*.tar
+
+#>>>>>>>>>>
+#tar xvf ${APT_CACHE_TAR_FILE} -C / &&
+#i have test this, but when apt-get install os will download all packages again
+
+apt-get update &&
+apt-get install -y software-properties-common &&
+add-apt-repository -y ppa:bitcoin/bitcoin
+apt-get update &&
+
+{ 
+sed -e 's|#.*$||g' ${APT_PACK_LIST_FILE} | xargs apt-get install -y; 
+sed -e 's|#.*$||g' ${APT_PACK_LIST_FILE} | xargs apt-get install -y;
+sed -e 's|#.*$||g' ${APT_PACK_LIST_FILE} | xargs apt-get install -y;
+} && 
+apt -y autoremove && 
+apt-get clean
+
+#######################################
+#######################################
+#######################################
+
+echo
+echo "##########"
+echo "$0 said: run ok."
+echo "##########"
+echo 
